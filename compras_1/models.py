06670a47692df1249652e0d716ae985dc7154f9f
Selva <selvamochen@hotmail.com>
2023-01-16 18:15:57 -0300

    compras_1/models.py

--- compras_1/models.py
+++ compras_1/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class Productos(models.Model):
     nombre = models.CharField(max_length=256)
 
+    def __str__(self):
+        return f"{self.nombre}"
+
 
 
 class Clientes(models.Model):
@@ -12,15 +15,24 @@ class Clientes(models.Model):
     dni = models.CharField(max_length=32)
     email = models.EmailField()
     fecha_nac = models.DateField()
+    
+    def __str__(self):
+        return f"{self.nombre}, {self.apellido}"
 
 class Estado_producto(models.Model):
     fecha_vencimiento = models.DateTimeField()
     fresco = models.BooleanField(default=False)
     congelado = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return f"{self.fresco}, {self.congelado}"
 
 
 class Comentarios(models.Model):
     comments = models.TextField()
+
+    def __str__(self):
+        return f"{self.comments}"
 
 
 
